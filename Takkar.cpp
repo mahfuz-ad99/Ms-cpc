e887819406510a208813080e535691b3444e6f04
mahfuz-ad99 <mafi6470@gmail.com>
2026-05-03 21:27:04 +0300

    Create Takkar.cpp

---
+++ Takkar.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+int a;
+int b;
+cin>>a>>b;
+if (a>b){
+cout<<"MAGA!";}
+else if (b>a){
+cout <<"FAKE NEWS!";}
+else if(a==b){
+cout<<"WORLD WAR 3!";}
+}
